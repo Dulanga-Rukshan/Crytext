cba54ad87c17973381e4652d6086eb7d807e4d71
Dulanga-Rukshan <dulangarukshan@proton.me>
2025-01-25 23:42:13 +0530

    added -f feature and file extension filter

--- crytext.py
+++ crytext.py
@@ -2,6 +2,8 @@ import base64
 import sys 
 import getopt 
 import os
+import getpass
+
 
 helpText = """
  CCCCC   RRRRR   Y  Y  TTTTTT  EEEEE  X   X  TTTTT
@@ -30,30 +32,34 @@ Description:
 
 Examples:
   Encrypt a file:
-    python crytext.py -e -f input.txt
+    python crytext.py -f input.txt -e 
 
   Decrypt a file:
-    python crytext.py -d -f encrypted.txt
+    python crytext.py -f encrypted.txt -d
 
   Show help:
     python crytext.py -h
 """
 
-toolArt = """
-  CCCCC   RRRRR   Y   Y  TTTTT  EEEEE   X   X  TTTTT
- C        R   R    Y Y     T    E        X X     T   
- C        RRRRR     Y      T    EEEE      X      T   
- C        R  R      Y      T    E        X X     T   
-  CCCCC   R   R     Y      T    EEEEE   X   X    T   
+def welcomeMsg(): 
+    toolArt = f"""
+      CCCCC   RRRRR   Y   Y  TTTTT  EEEEE   X   X  TTTTT
+     C        R   R    Y Y     T    E        X X     T   
+     C        RRRRR     Y      T    EEEE      X      T   
+     C        R  R      Y      T    E        X X     T   
+      CCCCC   R   R     Y      T    EEEEE   X   X    T   
 
-   ********CryText is a tool designed by Dulanga Rukshan. This tool designed for encryption and decryption of files or text.******** 
- 
-"""
+       ********CryText is a tool designed by Dulanga Rukshan. This tool designed for encryption and decryption of files or text.******** 
+
+     Hello {getpass.getuser()}!. Welcome to my simple base64 file encoding & decoding tool. I encourage you to submit a review to this email what should i include to this tool - dulangarukshan@proton.me 
+
+    """
+    return toolArt
 
 
 
 def main(support,asciiArt):
-    longOptions = ["filepath", "encrypt", "decrypt", "help", "image"]
+    longOptions = ["filepath", "encrypt", "decrypt", "help"]
     argumentsList = sys.argv[1:]
     #encrypt function
     def encodeText(y):
@@ -79,62 +85,71 @@ def main(support,asciiArt):
         except(base64.error, UnicodeDecodeError) as e:
             print(f"Error decoding: {e}")
             return
-            
 
+
+    def encodingFile(filepath,filename,fileextension):
+        try:
+            with open(encode, "r") as file:
+                print("Encrypting file...")
+                with open(f"{filename}Encrypted.{fileextension}", "w") as encryptFile:                                           
+                    for e in file:
+                        encryptFile.write(f"{encodeText(e)}\n")                
+            print(f"Encrypt file created!!!")
+        except Exception as e:
+            print(f"File named {filepath} has been encrypted as {filename}{fileextension}!!!")
+
+    def decodingFile(filepath, filename, fileextension):
+        try:
+            with open(filepath, "r") as file:
+                print("Decrypting file...")
+                with open(f"{filename}Decrypt{fileextension}", "w") as decryptFile:
+                    for d in file:
+                        decryptFile.write(f"{decodeText(d)}\n")
+            print(f"File named {filepath} has been decrypted as {filename}{fileextension}!!!")
+        except Exception as e:
+            print(f"Decrypting file error - {e}.")
+                  
     try:
-        arg, values = getopt.getopt(argumentsList, "f:e:d:h", longOptions)
+        arg, values = getopt.getopt(argumentsList, "f:edh", longOptions)
+        filePath = None
         for opt, args in arg:
+               if opt in ("-f", "--file"):
+                   filePath = args
+                    
                #to incrypt file using base64
-               if opt in ("-e","--encrypt"):
-                    try:
-                        print(asciiArt)
-                        with open(args, "r") as file:
-                            print("Encrypting file...")
-                            with open("encrypted.txt", "w") as encryptFile:
-                                for e in file:
-                                    encryptFile.write(f"{encodeText(e)}\n")                
-                        print(f"Encrypt file created!!!")
+               elif opt in ("-e","--encrypt") and filePath:
+                   if filePath:
+                       if os.path.exists(filePath):
+                           print(asciiArt)
+                           fileName, fileExtension = os.path.splitext(filePath)
+                           encodingFile(filePath, fileName, fileExtension)
+                       else:
+                           print(asciiArt)
+                           print(f"{filePath} not found!!!")            
  
-                    except FileNotFoundError:
-                        print(f"{args} : file not found.")
-                
+                   else:
+                       print("Error: file path is not specified! Use -f <file-path>.")
 
 
                #to decrypt file that been encrypt by base64 
-               elif opt in ("-d", "--decrypt"):
-                  #to decrypt file using base64
-                    try:
-                        print(asciiArt)
-                        if opt in ("-d", "--decrypt"):
-                            with open(args, "r") as file:
-                                print("Decrypting file...")
-                                with open("decrypt.txt", "w") as decryptFile:
-                                    for d in file:
-                                        decryptFile.write(f"{decodeText(d)}\n")
+               elif opt in ("-d", "--decrypt") and filePath:
+                   if filePath: 
+                       if os.path.exists(filePath):
+                           print(asciiArt)
+                           fileName,fileExtension = os.path.splitext(filePath)
+                           decodingFile(filePath,fileName, fileExtension)                            
+                       else:
+                           print(asciiArt)
+                           print(f"{filePath} not found!!!")
+                   else:
+                       print(f"Error file path is not specified! Use -f <file-path>.")
 
-                        elif opt in ("-i", "--image" and "-d", "--decrypt"):
-                            with open("decrypt.jpg", "w") as decryptFile:
-                                for d in file:
-                                    
-                        print("File has been decrypted!!!")
-
-                    except FileNotFoundError:
-                        print(f"{args} : file not found.")                    
-                
-               elif opt in ("-i", "--image"):
-                   try:
-                       print(asciiArt)
-                       with open(args, "rb") as image:
-                           print()
-
-               elif opt in ("-f", "--file"):
-                    print("File path is here.")
-               elif opt in ("help", "--help"):
-                    print("Help is here")
+               elif opt in ("-h", "--help"):
+                   print(support)
     except:
         print("Error")
 if __name__ == "__main__":
-    main(helpText,toolArt)
+    main(helpText,welcomeMsg())
 
 
 
